9829319c885f6269112c4c2383d3cffe1f61e0f2
Evgeniy <evgnec@gmail.com>
2023-04-11 17:47:01 +0300

    add

--- scripts/api.js
+++ scripts/api.js
@@ -1,4 +1,4 @@
-const ENDPOINT = "https://newsapi.org/v2/everything/b";
+const ENDPOINT = "https://newsapi.org/v2/everything";
 const API_KEY = "dd82ff3604224bf1b224da3ef75c9135";
 
 function getNews(query) {
